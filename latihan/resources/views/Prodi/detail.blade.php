d3df65f57a04195e73d3e5479b7d1841ef2bbbc9
alfaruqd <fariddanialfaruq15@gmail.com>
2025-05-11 07:19:22 +0700

    laravel

---
+++ latihan/resources/views/Prodi/detail.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.master')
+
+@section('title', 'Detail Prodi')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Detail Prodi</h3>
+        </div>
+        <div class="card-body">
+            <p>Nama Prodi: Teknik Informatika</p>
+            <p>Fakultas: Ilmu Komputer</p>
+        </div>
+    </div>
+@endsection
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\MateriController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\DosenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+    Route::resource('materi', MateriController::class);
+    Route::resource('prodi', ProdiController::class);
+    Route::resource('fakultas', FakultasController::class);
+    Route::resource('mhs', MahasiswaController::class);
+    Route::resource('dosen', DosenController::class);
+
+
+
